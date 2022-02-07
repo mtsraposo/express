d886193337bfb5c24668d28bf297a9acaf7a07f4
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-07 15:40:34 -0300

    serve static files from public directory

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/*
+To serve static files such as images, CSS files, and JavaScript files,
+use the express.static built-in middleware function in Express.
+The function signature is:
+  express.static(root, [options])
+The root argument specifies the root directory from which to serve
+static assets. For more information on the options argument,
+see express.static.
+For example, use the following code to serve images, CSS files,
+and JavaScript files in a directory named public:
+  app.use(express.static('public'))
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
