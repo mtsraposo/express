74559e1313080379d0ce8b5e2a6493a160b8e6ae
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-07 15:34:10 -0300

    add basic routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Respond to POST request on the root route (/), the application’s home page:
+router.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+// Respond to a PUT request to the /user route:
+router.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Respond to a DELETE request to the /user route:
+router.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
 module.exports = router;
