b0953a4327275d72b4781f3071d785b61e878ccd
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-08 16:55:43 -0300

    add nested router

--- routes/router_params.js
+++ routes/router_params.js
@@ -6,94 +6,72 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4001;
 
-const spiceRack = [
+app.use(bodyParser.json());
+
+const spiceRacks = [
     {
         id: 1,
-        name: 'cardamom',
-        grams: 45,
+        location: 'Kitchen Countertop',
     },
     {
         id: 2,
-        name: 'pimento',
-        grams: 20,
+        location: 'Pantry',
     },
     {
         id: 3,
-        name: 'cumin',
-        grams: 450,
-    },
-    {
-        id: 4,
-        name: 'sichuan peppercorns',
-        grams: 107,
+        location: 'Outdoor Barbecue',
     }
 ];
 
-let nextSpiceId = 5;
+let nextSpiceRackId = 4;
 
-app.use(bodyParser.json());
-
-// Add your code here:
-app.param('spiceId', (req, res, next, id) => {
-    let spiceId = Number(id);
-    const spiceIndex = spiceRack.findIndex(
-        spice => spice.id === spiceId
-    );
-    if (spiceIndex !== -1) {
-        req.spiceIndex = spiceIndex;
+app.param('spiceRackId', (req, res, next, id) => {
+    const idToFind = Number(id);
+    const rackIndex = spiceRacks.findIndex(spiceRack => spiceRack.id === idToFind);
+    if (rackIndex !== -1) {
+        req.spiceRack = spiceRacks[rackIndex];
+        req.spiceRackIndex = rackIndex;
         next();
     } else {
-        res.status(404).send();
+        res.status(404).send('Spice Rack Not Found.');
     }
 });
 
-
-app.get('/spices/', (req, res, next) => {
-    res.send(spiceRack);
+app.get('/spice-racks/', (req, res, next) => {
+    res.send(spiceRacks);
 });
 
-app.post('/spices/', (req, res, next) => {
-    const newSpice = req.body.spice;
-    if (newSpice.name  && newSpice.grams) {
-        newSpice.id = nextSpiceId++;
-        spiceRack.push(newSpice);
-        res.send(newSpice);
+app.post('/spice-racks/', (req, res, next) => {
+    const newRack = req.body.spiceRack;
+    newRack.id = nextSpiceRackId++;
+    spiceRacks.push(newRack);
+    res.send(newRack);
+});
+
+app.get('/spice-racks/:spiceRackId', (req, res, next) => {
+    res.send(req.spiceRack);
+});
+
+app.put('/spice-racks/:spiceRackId', (req, res, next) => {
+    const updatedRack = req.body.spiceRack;
+    if (req.spiceRack.id !== updatedRack.id) {
+        res.status(400).send('Cannot update Spice Rack Id');
     } else {
-        res.status(400).send();
+        spiceRacks[req.spiceRackIndex] = updatedRack;
+        res.send(spiceRacks[req.spiceRackIndex]);
     }
 });
 
-app.get('/spices/:spiceId', (req, res, next) => {
-    const spiceId = Number(req.params.id);
-    const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
-    if (spiceIndex !== -1) {
-        res.send(spiceRack[spiceIndex]);
-    } else {
-        res.status(404).send('Spice not found.');
-    }
+app.delete('/spice-racks/:spiceRackId', (req, res, next) => {
+    spiceRacks.splice(req.spiceRackIndex, 1);
+    res.status(204).send();
 });
 
-app.put('/spices/:spiceId', (req, res, next) => {
-    const spiceId = Number(req.params.id);
-    const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
-    if (spiceIndex !== -1) {
-        spiceRack[spiceIndex] = req.body.spice;
-        res.send(spiceRack[spiceIndex]);
-    } else {
-        res.status(404).send('Spice not found.');
-    }
-});
+const spicesRouter = require('./spicesRouter');
 
-app.delete('/spices/:spiceId', (req, res, next) => {
-    const spiceId = Number(req.params.id);
-    const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
-    if (spiceIndex !== -1) {
-        spiceRack.splice(spiceIndex, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).send('Spice not found.');
-    }
-});
+// Write your code below:
+app.use('/spice-racks/:spiceRackId/spices', spicesRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
